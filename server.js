a7fa8d9599690aea30399bfc778f51de186c5d60
Jason Sinn <jrs341@me.com>
2019-07-09 10:10:19 -0500

    removed htaccess and modified header

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 var express = require('express')
 var app = express()
 
+var options = {
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now()),
+    res.set('Cache-Control', 'public')
+  }
+}
+
 app.set('port', (process.env.PORT || 3000))
 
-app.use(express.static(__dirname + '/'))
+app.use(express.static(__dirname + '/', options))
 
 app.get('/index', function(request, response) {
-	response.writeHead(200,{'Cache-Control': private})
   response.send('index')
 })
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function(request, response) {
+  console.log('Node app is running on port', app.get('port'))
 })
