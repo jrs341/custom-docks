356545e0a7eb61ced5258443d9bb68b1204d48ec
Jason Sinn <jrs341@me.com>
2019-07-11 19:27:13 -0500

    updated the navigation and added shell pages

--- server.js
+++ server.js
@@ -16,6 +16,26 @@ app.get('/index', function(request, response) {
   response.send('index')
 })
 
+app.get('/about', function(request, response) {
+  response.send('about')
+})
+
+app.get('/contact', function(request, response) {
+  response.send('contact')
+})
+
+app.get('/docks', function(request, response) {
+  response.send('docks')
+})
+
+app.get('/lifts', function(request, response) {
+  response.send('lifts')
+})
+
+app.get('/services', function(request, response) {
+  response.send('services')
+})
+
 app.listen(app.get('port'), function(request, response) {
   console.log('Node app is running on port', app.get('port'))
 })
